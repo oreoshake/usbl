e1031731873ab8e86d68b2900a6fd17e22adeb7e
Neil Matatall <oreoshake@users.noreply.github.com>
2020-08-11 12:49:43 -1000

    fix firefox?

--- app.js
+++ app.js
@@ -28,33 +28,36 @@ window.usabilla ||
           b.id = "usabilla-" + g;
           /MSIE[ ]+6/.test(navigator.userAgent) && (b.src = "javascript:false");
           f.appendChild(b);
-          // try {
-          //   b.contentWindow.document.open();
-          // } catch (c) {
-          //   (e.domain = d.domain),
-          //     (a =
-          //       "javascript:var d=document.open();d.domain='" +
-          //       e.domain +
-          //       "';"),
-          //     (b.src = a + "void(0);");
-          // }
-          // try {
-          //   var l = b.contentWindow.document;
-          //   l.write(
-          //     [
-          //       "<!DOCTYPE html><html><head><\/head><body><script nonce=\"27f1992y\">window.onload = function () {var d = document;d.getElementsByTagName(\"head\")[0].appendChild(d.createElement(\"script\")).src='",
-          //       e.url,
-          //       "';};<\/script><\/body><\/html>",
-          //     ].join("")
-          //   );
-          //   l.close();
-          // } catch (m) {
-          //   b.src =
-          //     a +
-          //     'd.write("' +
-          //     loaderHtml().replace(/"/g, String.fromCharCode(92) + '"') +
-          //     '");d.close();';
-          // }
+          try {
+            b.contentWindow.document.open();
+          } catch (c) {
+            (e.domain = d.domain),
+              (a =
+                "javascript:var d=document.open();d.domain='" +
+                e.domain +
+                "';"),
+              (b.src = a + "void(0);");
+          }
+          try {
+            var l = b.contentWindow.document;
+
+            // Without this line, firefox fails with "Uncaught TypeError: b.contentWindow.document.body is null"
+            // It generates a violation but the code that follows continues working.
+            l.write(
+              [
+                "<!DOCTYPE html><html><head><\/head><body><script>window.onload = function () {var d = document;d.getElementsByTagName(\"head\")[0].appendChild(d.createElement(\"script\")).src='",
+                e.url,
+                "';};<\/script><\/body><\/html>",
+              ].join("")
+            );
+            l.close();
+          } catch (m) {
+            b.src =
+              a +
+              'd.write("' +
+              loaderHtml().replace(/"/g, String.fromCharCode(92) + '"') +
+              '");d.close();';
+          }
 
           newScript = d.createElement('script');
           newScript.type = 'text/javascript';
